070fbd7fc8f4fd1f14f818825372869858522b9e
pabloosegat <pablo.h.segat@gmail.com>
2024-09-05 10:39:00 -0300

    refactor: refaz a logica de retornar a lista de contas. de um modo estatico para uma consulta real ao banco de dados

--- contas_pagar_receber/routers/contas_pagar_receber_router.py
+++ contas_pagar_receber/routers/contas_pagar_receber_router.py
@@ -25,15 +25,8 @@ class ContaPagarReceberRequest(BaseModel):
 
 
 @router.get('', response_model=List[ContaPagarReceberResponse])
-def listar_contas():
-    return [
-        ContaPagarReceberResponse(id=1, desc='Conta de Luz', valor=100.00, tipo='Pagar'),
-        ContaPagarReceberResponse(id=2, desc='Conta de Agua', valor=50.00, tipo='Pagar'),
-        ContaPagarReceberResponse(id=3, desc='Conta de Internet', valor=200.00, tipo='Pagar'),
-        ContaPagarReceberResponse(id=4, desc='Conta de Telefone', valor=150.00, tipo='Pagar'),
-        ContaPagarReceberResponse(id=5, desc='Conta de Salario', valor=5000.00, tipo='Receber'),
-        ContaPagarReceberResponse(id=6, desc='Conta de Aluguel' , valor=5000.00, tipo='Pagar')
-    ]
+def listar_contas(db: Session=Depends(get_db)) -> List[ContaPagarReceberResponse]:
+    return db.query(ContaPagarReceber).all()
 
 @router.post('', response_model=ContaPagarReceberResponse, status_code=201)
 def criar_conta(conta: ContaPagarReceberRequest,
