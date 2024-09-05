649a965172afbc799702061444f26bf43db22705
pabloosegat <pablo.h.segat@gmail.com>
2024-09-05 10:59:33 -0300

    refactor: modifica a estrutura do codigo para uma melhor praticidade

--- shared/database.py
+++ shared/database.py
@@ -1,13 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-db_user = 'millenium_falcon'
-db_password = 'x_wing'
-db_host = 'localhost'
-db_port = 5432
-db_name = 'contas_pagar_receber'
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+db_name = os.getenv('DB_NAME')
 
 DATABASE_URL = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
 
