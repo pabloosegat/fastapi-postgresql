693210e6096b6f5cfb03127f4b11a31c7c3fa258
pabloosegat <pablo.h.segat@gmail.com>
2024-09-06 11:45:17 -0300

    fix: separa arquivos de gerenciamento de excessoes e do app, tambem resolve questoes de dependencia entre eles

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import uvicorn
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI
 
 from routers import contas_pagar_receber_router
 from shared.exceptions import NotFound
+from shared.exceptions_handler import not_found_exception_handler
 
 
 app = FastAPI()
@@ -15,13 +15,7 @@ def hello_world() -> dict:
     return {'message': 'Hello, World!'}
 
 app.include_router(contas_pagar_receber_router.router)
-
-@app.exception_handler(NotFound)
-async def not_found_excepion_handler(request: Request, exc: NotFound):
-    return JSONResponse(
-        status_code=404,
-        content={'message': f'Oops! {exc.name} não encontrada!'}
-    )
+app.add_exception_handler(NotFound, not_found_exception_handler)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='0.0.0.0', port=8001, reload=True)
