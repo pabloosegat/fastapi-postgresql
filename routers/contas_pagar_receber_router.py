e1807f8abea8eb9a3ef8982827af3909b1eebbff
pabloosegat <pablo.h.segat@gmail.com>
2024-09-06 09:01:23 -0300

    feat: adiciona validacoes a requisicao ContasPagarReceberRequest

--- routers/contas_pagar_receber_router.py
+++ routers/contas_pagar_receber_router.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
-from pydantic import BaseModel
+from decimal import Decimal
+from enum import Enum
 from typing import List
+
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel, Field
+from sqlalchemy.orm import Session
 
 from shared.dependencies import get_db
 from models.contas_pagar_receber_model import ContaPagarReceber
@@ -12,16 +15,20 @@ router = APIRouter(prefix='/contas-pagar-receber')
 class ContaPagarReceberResponse(BaseModel):
     id: int
     desc: str
-    valor: float
+    valor: Decimal
     tipo: str
     
     class Config:
         orm_mode = True
 
+class ContaPagarReceberTipoEnum(str, Enum):
+    PAGAR = 'pagar'
+    RECEBER = 'receber'
+
 class ContaPagarReceberRequest(BaseModel):
-    desc: str
-    valor: float
-    tipo: str
+    desc: str = Field(min_length=3, max_length=30)
+    valor: Decimal = Field(gt=0, )
+    tipo: ContaPagarReceberTipoEnum
 
 
 @router.get('', response_model=List[ContaPagarReceberResponse])
