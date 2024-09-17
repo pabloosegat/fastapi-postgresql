f0e9034a6a9e1fe4484e9d6d5529bddadc2f277d
pabloosegat <pablo.h.segat@gmail.com>
2024-09-17 17:00:32 -0300

    feat: endpoint de acao para baixar contas

--- models/contas_pagar_receber_model.py
+++ models/contas_pagar_receber_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Numeric, String, ForeignKey
+from sqlalchemy import Column, Integer, Numeric, String, ForeignKey, DateTime, Boolean
 from sqlalchemy.orm import relationship
 from shared.database import Base
 
@@ -9,6 +9,9 @@ class ContaPagarReceber(Base):
     desc = Column(String(30))
     valor = Column(Numeric)
     tipo = Column(String(30))
+    data_baixa = Column(DateTime())
+    valor_baixa = Column(Numeric)
+    esta_baixada = Column(Boolean, default=False)
     
     id_fornecedor_cliente = Column(Integer, ForeignKey('tbl_fornecedor_cliente.id'))
     fornecedor_cliente = relationship('FornecedorCliente')
