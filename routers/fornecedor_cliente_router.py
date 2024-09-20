d8f29d62c2b674b1ea934de0805babec767e7755
pabloosegat <pablo.h.segat@gmail.com>
2024-09-20 09:57:47 -0300

    feat: adiciona summary e description para cada um dos endpoints

--- routers/fornecedor_cliente_router.py
+++ routers/fornecedor_cliente_router.py
@@ -23,7 +23,11 @@ class FornecedorClienteRequest(BaseModel):
 # CRUD
 
 # Create
-@router.post('', response_model=FornecedorClienteResponse)
+@router.post('', 
+    response_model=FornecedorClienteResponse,
+    summary="Criar Fornecedor/Cliente",
+    description="Cria um novo fornecedor ou cliente com o nome fornecido."
+)
 def criar_fornecedor_cliente(fornecedor_cliente: FornecedorClienteRequest,
                             db: Session=Depends(get_db)) -> FornecedorClienteResponse:
     
@@ -36,18 +40,30 @@ def criar_fornecedor_cliente(fornecedor_cliente: FornecedorClienteRequest,
     return for_cli
 
 # Read
-@router.get('', response_model=List[FornecedorClienteResponse])
+@router.get('',
+    response_model=List[FornecedorClienteResponse],
+    summary="Listar Fornecedores/Clientes",
+    description="Retorna uma lista de todos os fornecedores e clientes."
+)
 def listar_fornecedor_cliente(db: Session=Depends(get_db)) -> List[FornecedorClienteResponse]:
     return db.query(FornecedorCliente).all()
 
-@router.get('/{id_fornecedor_cliente}', response_model=FornecedorClienteResponse)
+@router.get('/{id_fornecedor_cliente}',
+    response_model=FornecedorClienteResponse,
+    summary="Obter Fornecedor/Cliente por ID",
+    description="Retorna um fornecedor ou cliente específico pelo seu ID."
+)
 def listar_um_fornecedor_cliente(id_fornecedor_cliente: int,
                                 db: Session=Depends(get_db)) -> FornecedorClienteResponse:
 
     return consultar_fornecedor_cliente_por_id(id_fornecedor_cliente, db)
 
 # Update
-@router.put('/{id_fornecedor_cliente}', status_code=200)
+@router.put('/{id_fornecedor_cliente}',
+    status_code=200,
+    summary="Atualizar Fornecedor/Cliente",
+    description="Atualiza os detalhes de um fornecedor ou cliente existente."
+)
 def atualizar_fornecedor_cliente(id_fornecedor_cliente: int,
                                 fornecedor_cliente_request: FornecedorClienteRequest,
                                 db: Session=Depends(get_db)) -> FornecedorClienteResponse:
@@ -63,7 +79,11 @@ def atualizar_fornecedor_cliente(id_fornecedor_cliente: int,
     return for_cli
 
 # Delete
-@router.delete('/{id_fornecedor_cliente}', status_code = 204)
+@router.delete('/{id_fornecedor_cliente}',
+    status_code = 204,
+    summary="Deletar Fornecedor/Cliente",
+    description="Remove um fornecedor ou cliente do sistema pelo seu ID."
+)
 def deletar_fornecedor_cliente(id_fornecedor_cliente: int,
                             db: Session=Depends(get_db)):
 
